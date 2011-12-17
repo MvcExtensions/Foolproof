fee041e3386d00d4cb116897c7b6effcd5691e4e
hazzik <hazzik@gmail.com>
2011-12-17 15:05:08 +0600

    Changed product to MvcExtensions.Foolproof

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild
 set configuration=%1
 if "%configuration%" == "" set configuration=Release
 
-%msbuild% MvcExtensions.build /t:Full /p:Configuration=%configuration% /m:2
+%msbuild% MvcExtensions.Foolproof.build /t:Full /p:Configuration=%configuration% /m:2
 
 pause
